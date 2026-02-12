c80f91d2f871ed4f3ac4fc4f2b3f0d07f337c0df
55DevDanilo <dev.danilo_asl@outlook.com>
2026-02-12 19:03:35 -0300

    Criando a classe department e testando funcionalidade da classe na classe main

---
+++ demo_dao_jdbc/src/application/Principal.java
@@ -0,0 +1,14 @@
+package application;
+
+import module.entities.Department;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1, "books");
+		System.out.print(obj);
+
+	}
+
+}
