7c37d1b7942e8634bbe30eb7a9361f9e24f94426
55DevDanilo <dev.danilo_asl@outlook.com>
2026-02-12 19:11:36 -0300

    Criando a classe Seller

--- demo_dao_jdbc/src/application/Principal.java
+++ demo_dao_jdbc/src/application/Principal.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import module.entities.Department;
+import module.entities.Seller;
 
 public class Principal {
 
@@ -8,6 +11,9 @@ public class Principal {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1, "books");
 		System.out.print(obj);
+		
+		Seller obj1 = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,obj);
+		System.out.print(obj1);
 
 	}
 
